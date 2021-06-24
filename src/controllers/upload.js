ad864492c3bc5ec9f22f65aaabdfa31eadb04d9c
Fernando Fernandes <fernandofitilan@hotmail.com>
2021-06-24 09:54:17 -0300

    up

---
+++ src/controllers/upload.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+const db = require("../models");
+const Image = db.images;
+
+const uploadFiles = async (req, res) => {
+  try {
+    console.log(req.file);
+
+    if (req.file == undefined) {
+      return res.send(`You must select a file.`);
+    }
+
+    Image.create({
+      type: req.file.mimetype,
+      name: req.file.originalname,
+      data: fs.readFileSync(
+        __basedir + "/resources/static/assets/uploads/" + req.file.filename
+      ),
+    }).then((image) => {
+      fs.writeFileSync(
+        __basedir + "/resources/static/assets/tmp/" + image.name,
+        image.data
+      );
+
+      return res.send(`Imagem upada com sucesso.`);
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send(`Error quando tentou subir a imagem: ${error}`);
+  }
+};
+
+module.exports = {
+  uploadFiles,
+};
